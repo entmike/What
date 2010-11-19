60e1e17a273587b4c30c23e7adb12c89f962c7f9
U-Mike-PC\Mike <Mike@Mike-PC.(none)>
2010-11-18 23:24:01 -0600

    Sample hosts.js cleanup

--- conf/hosts.js
+++ conf/hosts.js
@@ -4,24 +4,16 @@
 	hosts : [
 		{
 			name : "entmike.com",
+			appBase : "/var/www",
+			defaultHost : false,	
+			aliases : ["www.entmike.com", "localhost"],
+			translations : [
+				{ target : "/index.html", source : ["/", "index.htm"] }
+			]
+		},{
+			name : "defaulthost",
 			appBase : "websites/defaulthost",
-			allowDirectoryListing : true,
-			defaultHost : true,	
-			aliases : ["www.entmike.com"],
-			adminApp : "manager",
-			translations : [
-				{ target : "/index.html", source : ["/", "index.htm"] },
-				{ target : "/Admin/", source : ["/Admin"] }
-			]
-		}/*,{
-			name : "omgwtflol.com",
-			appBase : "websites/omgwtflol.com",
-			adminApp : "manager",
-			allowDirectoryListing : false,
-			aliases : ["www.omgwtflol.com"],
-			translations : [
-				{ target : "/manager/", source : ["/manager"] }
-			]
-		}*/
+			defaultHost : true
+		}
 	]
 }
